d271831b54055d35f47f9aee2b82132684d3c176
BT_1N3_01@BKTECH.LOCAL <BT_1N3_01@BKTECH.LOCAL>
2018-10-01 08:42:02 -0400

    Initial commit

---
+++ src/TazwarBelal/Main.java
@@ -0,0 +1,9 @@
+package TazwarBelal;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+System.out.println(+TazwarBelalLib("racecar"));
+    }
+}
---
+++ src/TazwarBelal/TazwarBelalLib.java
@@ -0,0 +1,21 @@
+package TazwarBelal;
+
+public class TazwarBelalLib{
+    public static String isPalindrome(String str)
+    {
+
+        int i = 0;
+ for (i=0;i < str.length(); i++){
+        if (str.substring(str.length()) = str.substring(str.length()-1,i++))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
+}
+
