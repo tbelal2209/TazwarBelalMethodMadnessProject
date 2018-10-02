bb1892c5bd9b6d7de6ef7017aa628725982f5e6d
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-10-02 08:43:53 -0400

    Update TazwarBelalLib.java

--- src/TazwarBelal/TazwarBelalLib.java
+++ src/TazwarBelal/TazwarBelalLib.java
@@ -1,16 +1,15 @@
 package TazwarBelal;
 
-public class isPalindromeLib{
-    public static boolean isPalindrome(String str)
-    {
+public class isPalindromeLib {
+    public static boolean isPalindrome(String str) {
 
         int i = 0;
- for (i=0;i < str.length(); i--){
-        if (str.substring(str.length()) == str.substring(str.length()-1,i--))
-        {
-            return true;
+        for (i = 0; i < str.length(); i--) {
+            if (str.substring(str.length()) == str.substring(str.length() - 1, i--)) {
+                return true;
+            } else {
+                return false;
+            }
         }
-
-        return false;
     }
 }
