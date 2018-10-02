b376d310e6ca48f82944ffdbe4703622896895fb
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-10-02 08:36:54 -0400

    Update Main.java

--- src/TazwarBelal/Main.java
+++ src/TazwarBelal/Main.java
@@ -1,9 +1,11 @@
-package TazwarBelal;
+package TazwarBelalM;
+
+import TazwarBelal.isPalindromeLib;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-System.out.println(+TazwarBelalLib("racecar"));
+        System.out.println("isPalindrome(racecar) = " + isPalindromeLib.isPalindrome("racecar"));
     }
 }
